af80fc426598c9ea89edf4f41e5c91c86a61546f
Chuturica <chuturica@gmail.com>
2022-01-27 13:55:04 +0100

    end of Webpack and vue-cli (Ep 16)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-
+    <message>Hello World</message>
+    <message>Hello World ponovo</message>
   </div>
 </template>
 
 <script>
+import Message from './components/Message.vue';
+
 export default {
   name: 'app',
+  components: { Message},
   data () {
     return {
 
